b1aff36a4251a862ffbf7971791729430f0f0e7c
jumi1234 <farewellsad@gmail.com>
2019-11-15 11:45:32 +0900

    mainscreen
    mainscreen

--- Biants/MainScreen.js
+++ Biants/MainScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Platform, Image, Button } from 'react-native';
 import { Icon } from 'native-base';
-import { createBottomTabNavigator, createAppContainer, createStackNavigator, StackActions } from 'react-navigation';
+import { createBottomTabNavigator, createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation';
 import HomeTab from './AppTabNavigator/HomeTab';
 import MessageTab from './AppTabNavigator/MessageTab';
 import ProfileTab from './AppTabNavigator/ProfileTab';
@@ -10,40 +10,39 @@ import ChatTab from './AppTabNavigator/ChatTab';
 import Login from './AppTabNavigator/Login';
 
 const AppTabNavigator = createBottomTabNavigator({
-  HomeTab: { screen: HomeTab,
-    navigationOptions: {
-        title: '',
-        tabBarIcon: ({ tintColor }) => (
-            <Icon name='ios-home' style={{ color: '#BDBDBD' }} />
-            // //<Image source={require('../com.jpg')}/>
-        )
-    }
- },
-  MessageTab: { screen: MessageTab,
-    navigationOptions: {
-        title: '',
-          tabBarIcon: ({ tintColor }) => (
-              <Icon name='ios-paper' style={{ color: '#BDBDBD' }} />
-          )
-      }
+  HomeTab: {
+    screen: HomeTab,
+  },
+  Board: {
+    screen: Board
+  },
+  MessageTab: {
+    screen: MessageTab,
    },
   ProfileTab: {
-     screen: Platform.OS === 'android' ? Login : ProfileTab }
+     screen: ProfileTab }
 });
 
-const AppTabContainet = createAppContainer(AppTabNavigator);
+export default createAppContainer(AppTabNavigator);
 
-export default class MainScreen extends Component {
+class MainScreen extends Component {
 
   static navigationOptions = {
-    title: 'CUCKOO',
+    headerTitle: (
+        <View style={{flex:1, flexDirection:'row', justifyContent:'center'}}>
+            <Image
+                source={require('./AppTabNavigator/img/cukcoo_logo.png')}
+                style={{width:170, height:45}}
+            />
+        </View>
+    )
     //headerLeft: <Image source={require('./MainLogo.png')} style={{width:200, height:40}}/>
     //headerRight: <Text style={{ paddingRight:10, fontFamily: Fonts.koverwatch }}>테스트</Text>,
   }
 
   render() {
-    return <AppTabContainet/>; (
-      <View style={styles.container}>
+    return  (
+      <View style={style.container}>
         <Text>main</Text>
       </View>
     );
