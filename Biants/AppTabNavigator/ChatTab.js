5d06ba997b0e878c3fdf9eb83169706fec8ce491
jumi1234 <farewellsad@gmail.com>
2019-11-19 11:07:13 +0900

    chatTab
    chatTab UI(proceeding)

--- Biants/AppTabNavigator/ChatTab.js
+++ Biants/AppTabNavigator/ChatTab.js
@@ -105,7 +105,10 @@ export default class ChatTab extends React.Component {
         sender: firebase.auth().currentUser.email,
         dateTime: dateTime,
         id: collectionId,
-        talker: [firebase.auth().currentUser.email, replyReceiver]
+        talker: [firebase.auth().currentUser.email, replyReceiver],
+        check: this.props.navigation.getParam('check', 'no'),
+        myInfo: this.props.navigation.getParam('myInfo', 'no'),
+        yourInfo: this.props.navigation.getParam('yourInfo', 'no'),
       });
 
       this.props.navigation.dispatch(StackActions.reset({
@@ -116,25 +119,27 @@ export default class ChatTab extends React.Component {
 
     }
 
-
 handleSubmit = () => {
 const word = {
 word: this.state.word,
 weight: this.state.weight
 }
 this._post(word);
-const pushAction = StackActions.push({
-routeName: 'HomeTab',
-  params: {
-    myUserId: 9,
-  },
-});
-this.props.navigation.dispatch(pushAction);
+// const pushAction = StackActions.push({
+// routeName: 'HomeTab',
+//   params: {
+//     myUserId: 9,
+//   },
+// });
+// this.props.navigation.dispatch(pushAction);
+this.props.navigation.goBack();
 }
 
 
-
     render() {
+      var age = this.props.navigation.getParam('age', 'no');
+      var region = this.props.navigation.getParam('region', 'no');
+      var email = firebase.auth().currentUser.email;
       if(this.state.isSender != 1) {
       return (
         <View style={styles.container}>
@@ -144,7 +149,10 @@ this.props.navigation.dispatch(pushAction);
               const message = this.state.messages[id];
               return (
                 <View style={styles.chatContainer} key={id}>
-                    <Text style={styles.reply}>{message.message}</Text>
+                    <Text style={message.sender == email ? styles.infoofme : styles.info}>[{region}/{age}세]</Text>
+                    <View style={message.sender == email ? styles.ballonofme : styles.ballon}>
+                      <Text style={message.sender == email ? styles.replyofme : styles.reply}>{message.message}</Text>
+                    </View>
                 </View>
               );
             })}
@@ -159,9 +167,9 @@ this.props.navigation.dispatch(pushAction);
             </View>
             <View style={styles.sendContainer}>
               <TouchableHighlight
-              underlayColor={'#4e4273'}
+              underlayColor={'#cda8d2'}
               onPress={() => this.onSendPress()}>
-                <Text style={styles.sendLabel}>보내기</Text>
+                <Text style={styles.sendLabel}>전송</Text>
               </TouchableHighlight>
             </View>
           </View>
@@ -176,15 +184,21 @@ this.props.navigation.dispatch(pushAction);
               const message = this.state.messages[id];
               return (
                 <View style={styles.chatContainer} key={id}>
-                    <Text style={styles.reply}>{message.message}</Text>
+                  <Text style={message.sender == email ? styles.infoofme : styles.info}>[{region}/{age}세]</Text>
+                  <View style={message.sender == email ? styles.ballonofme : styles.ballon}>
+                    <Text style={message.sender == email ? styles.replyofme : styles.reply}>{message.message}</Text>
+                  </View>
                 </View>
               );
             })}
           </View>
           </ScrollView>
           <View style={styles.inputContainer}>
-            <View style={styles.alreadytextContainer}>
-              <Text style={styles.text}>이미 쪽지를 보냈습니다.</Text>
+            <View style={styles.textContainer}>
+              <Text style={styles.inputin}>이미 쪽지를 보냈습니다</Text>
+            </View>
+            <View style={styles.sendContainerin}>
+                <Text style={styles.sendinactive}>전송</Text>
             </View>
           </View>
         </View>
@@ -194,77 +208,168 @@ this.props.navigation.dispatch(pushAction);
 }
 
 var styles = StyleSheet.create({
-container: {
-flex: 1,
-justifyContent: 'center',
-alignItems: 'stretch',
-backgroundColor: '#ffffff'
-},
-topContainer: {
-flex: 1,
-flexDirection: 'row',
-justifyContent: 'flex-start',
-alignItems: 'center',
-backgroundColor: 'black',
-paddingTop: 20,
-},
-chatContainer: {
-flex: 11,
-justifyContent: 'center',
-alignItems: 'stretch'
-},
-inputContainer: {
-flex: 1,
-flexDirection: 'row',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#efefef'
+  },
+  topContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    paddingTop: 20,
+  },
+  chatContainer: {
+    flex: 11,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    margin: 10,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#ffffff'
+  },
+  textContainer: {
+    flex: 0.9,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    width: '90%'
+  },
+  alreadytextContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  message: {
+    alignItems: 'flex-end',
+    backgroundColor: 'gray'
+  },
+  infoofme: {
+    display: 'none',
+  },
+  info: {
+    color: 'black',
+    fontSize: 13,
+  },
+  ballonofme: {
+    alignItems: 'flex-end',
+    padding: 15,
+  },
+  ballon: {
+    alignItems: 'flex-start',
+    padding: 5,
+  },
+  replyofme: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#e7c1ec',
+    borderColor: '#e7c1ec',
+    borderWidth: 1,
+    borderRadius: 8,
+    borderStyle: 'solid',
+    fontSize: 14,
+    padding: 10,
+  },
+  reply: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderColor: '#ffffff',
+    borderWidth: 1,
+    borderRadius: 8,
+    borderStyle: 'solid',
+    fontSize: 14,
+    padding: 10,
+  },
+  sendContainer: {
+    flex: 0.1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    paddingRight: 10,
+    backgroundColor: '#cda8d2',
+    borderColor: '#cda8d2',
+    borderWidth: 1,
+    borderRadius: 21,
+    borderStyle: 'solid',
+    height: '70%',
+    width: '10%'
+  },
+  sendLabel: {
+    color: '#ffffff',
+    fontSize: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginLeft: 5,
+  },
+  input: {
+    color: '#a8a8a8',
+    paddingRight: 10,
+    paddingLeft: 10,
+    paddingTop: 5,
+    height: 32,
+    borderColor: '#cda8d2',
+    borderWidth: 1,
+    borderRadius: 22,
+    borderStyle: 'solid',
 
-alignItems: 'center',
-backgroundColor: 'black'
-},
-textContainer: {
-flex: 1,
-justifyContent: 'center'
-},
-alreadytextContainer: {
-flex: 1,
-flexDirection: 'row',
-justifyContent: 'center'
-},
-message: {
-alignItems: 'flex-end',
-backgroundColor: 'gray'
-},
-reply: {
-flex: 1,
-justifyContent: 'center',
-backgroundColor: 'white'
-},
-sendContainer: {
-justifyContent: 'flex-end',
-paddingRight: 10
-},
-sendLabel: {
-color: '#ffffff',
-fontSize: 15
-},
-input: {
-  width: 300,
-color: 'black',
-paddingRight: 10,
-paddingLeft: 10,
-paddingTop: 5,
-height: 32,
-borderColor: 'black',
-borderWidth: 1,
-borderRadius: 2,
-alignSelf: 'center',
-backgroundColor: '#ffffff'
-},
-scrollview: {
-  height: '80%'
-},
-text: {
-  flex: 1,
-  justifyContent: 'center',
-  color: 'white'
-}
+    width:'95%',
+    height: '70%',
+    fontSize:15,
+
+    alignSelf: 'center',
+    backgroundColor: '#ffffff'
+  },
+  scrollview: {
+    height: '80%'
+  },
+  text: {
+    flex: 1,
+    justifyContent: 'center',
+    color: 'white'
+  },
+  inputin: {
+    paddingRight: 10,
+    paddingLeft: 10,
+    paddingTop: 5,
+    height: 32,
+    borderColor: '#efefef',
+    borderWidth: 1,
+    borderRadius: 22,
+    borderStyle: 'solid',
+    width:'95%',
+    height: '70%',
+    fontSize:15,
+
+    alignSelf: 'center',
+    backgroundColor: '#ffffff'
+  },
+  sendContainerin: {
+    flex: 0.1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    paddingRight: 10,
+    backgroundColor: '#a8a8a8',
+    borderColor: '#a8a8a8',
+    borderWidth: 1,
+    borderRadius: 21,
+    borderStyle: 'solid',
+    height: '70%',
+    width: '10%'
+  },
+  sendinactive: {
+    color: '#ffffff',
+    fontSize: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    marginLeft: 5,
+  },
 });
