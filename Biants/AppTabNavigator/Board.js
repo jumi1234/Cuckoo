9f1c6c22a180b720c1e00424d28b81aa05fb61ba
jumi1234 <farewellsad@gmail.com>
2019-11-20 18:06:53 +0900

    Update Board.js
    Board

--- Biants/AppTabNavigator/Board.js
+++ Biants/AppTabNavigator/Board.js
@@ -39,6 +39,7 @@ export default class Board extends React.Component {
       region: '',
       gender: '',
       users: {},
+      postavailable: '',
     };
   }
 
@@ -86,7 +87,6 @@ export default class Board extends React.Component {
       .then(querySnapshot => {
         const data = querySnapshot.docs.map(doc => doc.data());
             this.setState({users: data});
-            console.log(this.state.users);
             this.setState({age: data[0].age});
             this.setState({region: data[0].region});
             this.setState({gender: data[0].gender});
@@ -125,21 +125,58 @@ export default class Board extends React.Component {
   componentDidMount() {
     var emailad = firebase.auth().currentUser.email;
     this._get();
+
+  firebase.firestore().collection('words').where('id', '==', firebase.auth().currentUser.email).orderBy('dateTime', 'desc').limit(1)
+    .get()
+    .then(querySnapshot => {
+      const data = querySnapshot.docs.map(doc => doc.data());
+      const keys = Object.keys(data);
+
+      const k = keys[0];
+      var time = data[k].dateTime;
+
+      var yyyy = time[0] + time[1] + time[2] + time[3];
+      var mm = time[4] + time[5];
+      var dd = time[6] + time[7];
+      var h = time[8] + time[9];
+      var m = time[10] + time[11];
+      var s = time[12] + time[13];
+
+      var date = new Date(yyyy, mm, dd, h, m, s);
+      var a = date.getHours() + 12;
+      date.setHours(a);
+
+      var day = date.getDate(); //Current Date
+      if(day < 10) {
+        day = '0' + new Date().getDate(); //Current Minutes
+      }
+
+      var mon = date.getMonth(); //Current Month
+      if(mon < 10) {
+        mon = '0' + date.getMonth();
+      }
+      var y = date.getFullYear(); //Current Year
+
+      var hr = date.getHours(); //Current Hours
+      if(hr < 10) {
+        hr = '0' + date.getHours();
+      }
+      var mm = date.getMinutes(); //Current Minutes
+      if(mm < 10) {
+        mm = '0' + date.getMinutes(); //Current Minutes
+      }
+      var ss = date.getSeconds(); //Current Seconds
+      if(ss < 10) {
+        ss = '0' + date.getSeconds();
+      }
+
+      var postavailable = y.toString() + mon + day + hr + mm + ss;
+
+      this.setState({postavailable: postavailable});
+  });
   }
 
 handleSubmit = () => {
-  if(this.state.word == null || ! this.state.word) {
-    alert('내용을 입력하세요');
-  } else {
-  var user = firebase.auth().currentUser;
-  var email;
-  var userAge;
-  var userRegion;
-  var userSex;
-
-  if (user != null) {
-  email = user.email;
-  }
 
   var date = new Date().getDate(); //Current Date
   if(date < 10) {
@@ -162,7 +199,25 @@ handleSubmit = () => {
   if(sec < 10) {
     sec = '0' + new Date().getSeconds();
   }
+
   var dateTime = year.toString() + month + date + hours + min + sec;
+
+  if(this.state.word == null || ! this.state.word) {
+    alert('내용을 입력하세요');
+  } else {
+    if(dateTime < this.state.postavailable ) {
+      alert('12시간 이후 등록 가능합니다\n' + this.state.postavailable[4] + this.state.postavailable[5] + "/" + this.state.postavailable[6] + this.state.postavailable[7] + ' ' +
+      this.state.postavailable[8] + this.state.postavailable[9] + ":" + this.state.postavailable[10] + this.state.postavailable[11] + "~");
+    } else {
+  var user = firebase.auth().currentUser;
+  var email;
+  var userAge;
+  var userRegion;
+  var userSex;
+
+  if (user != null) {
+  email = user.email;
+  }
 
   firebase.firestore().collection('words').add({
     word: this.state.word,
@@ -180,23 +235,27 @@ handleSubmit = () => {
   }));
   }
 }
+}
 
 render(){
     return(
       <View style={style.container}>
-      <Textarea
-          name="word"
-          value={this.state.word}
-          containerStyle={style.textareaContainer}
-          style={style.textarea}
-          maxLength={70}
-          placeholder={'내용을 입력하세요'}
-          placeholderTextColor={'#c7c7c7'}
-          underlineColorAndroid={'transparent'}
-          onChangeText={(text) => this.setState({word: text})}
-          />
-        <View style={style.btn}>
-          <Button title="등록" color="#F15382" onPress={this.handleSubmit} style={{padding: 10}}/>
+        <View style={style.registerContainer}>
+          <View style={style.area}>
+            <Textarea
+                name="word"
+                value={this.state.word}
+                style={style.textarea}
+                maxLength={70}
+                placeholder={'내용을 입력하세요. (70자 내외)'}
+                placeholderTextColor={'#c7c7c7'}
+                underlineColorAndroid={'transparent'}
+                onChangeText={(text) => this.setState({word: text})}
+            />
+          </View>
+          <View style={style.btn}>
+            <Button title="등록" style={{width: 400, height:150}} color="#8c378b" onPress={this.handleSubmit} style={{padding: 10}}/>
+          </View>
         </View>
       </View>
     )
@@ -207,27 +266,41 @@ render(){
 const style = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
       justifyContent: 'center',
-      borderTopWidth: 12,
-      borderRightWidth: 12,
-      borderLeftWidth: 12,
-      borderBottomWidth: 12,
-      borderColor: '#efefef',
+      alignItems: 'center',
+      backgroundColor: '#f2e0f5',
     },
-    textareaContainer: {
-      height: '70%',
-      padding: 0,
+    registerContainer: {
       backgroundColor: '#FFFFFF',
+      padding: 10,
+      width: '95%',
+      height: '95%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderWidth: 1,
+      borderStyle: 'solid',
+      borderColor: '#8c378b',
+    },
+    area: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width:'80%',
+      height: 500,
+      fontSize:15,
+      marginTop: 200,
     },
     textarea: {
-      height: 180,
-      fontSize: 15,
+      flex: 1,
+      fontSize: 17,
       fontWeight: 'bold',
-      textAlign: 'center'
+      textAlign: 'center',
+      height: 400,
     },
     btn: {
-      padding: 20,
+      flex: 1,
+      marginTop: 200,
       width: '90%',
     },
 });
